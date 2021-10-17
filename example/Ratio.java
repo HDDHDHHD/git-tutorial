1e2a7091c7f6dd3cc8b7ade1f56841eac25e78a7
HDDHDHHD <74154817+HDDHDHHD@users.noreply.github.com>
2021-10-17 15:38:05 -0400

    Implemented Ratio.java

--- example/Ratio.java
+++ example/Ratio.java
@@ -1,5 +1,10 @@
 public class Ratio {
   public static double calculate(double width, double height) {
-    // Implement Me
+    if(width > height){
+      return width/height;
+    }
+    else{
+      return height/width;
+    }
   }
 }
